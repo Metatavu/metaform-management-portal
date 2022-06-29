de4f4465fe1abc0168c476faadff8e09e9635a72
Eetu Lepistö <bodom02@gmail.com>
2022-06-29 12:43:59 +0300

    Added test-json

--- src/components/screens/editor/draft-editor-screen.tsx
+++ src/components/screens/editor/draft-editor-screen.tsx
@@ -8,6 +8,7 @@ import NavigationTab from "components/layouts/navigations/navigation-tab";
 import strings from "localization/strings";
 import { Preview, Public, Save } from "@mui/icons-material";
 import { IconActionButton } from "styled/layouts/app-layout";
+import FormJson from "1c9d4662-886b-4832-84ea-34ca05f90932.json";
 
 /**
  * Draft editor screen component
@@ -15,7 +16,7 @@ import { IconActionButton } from "styled/layouts/app-layout";
 const DraftEditorScreen: React.FC = () => {
   // TODO draft fetch, set logic, replace the example form
   const editorRef = React.useRef<HTMLDivElement>(null);
-  const [ draftForm, setDraftForm ] = React.useState<Metaform>(MetaformUtils.jsonToMetaform({}));
+  const [ draftForm, setDraftForm ] = React.useState<Metaform>(MetaformUtils.jsonToMetaform(FormJson));
 
   /**
    * Renders draft editor actions
