3632fd47eb9f95ebbf9dced09e78edd2b86872c5
Otto Hooper <otto.hooper@metatavu.fi>
2022-07-13 12:00:54 +0300

    Create a component for selector. Disable second selector if first is enabled

---
+++ src/components/selectors/user-selector.tsx
@@ -0,0 +1,53 @@
+import React from "react";
+import { Selector, FormControlWrapper } from "../../styled/layouts/admin-layout";
+
+/**
+ * Disable second selector by default and if first selector is selected, enable second selector
+*/
+const UserSelector: React.FC = () => {
+  const [firstSelector, setFirstSelector] = React.useState<string>("");
+  const [secondSelector, setSecondSelector] = React.useState<string>("");
+
+  // eslint-disable-next-line require-jsdoc
+  const handleFirstSelectorChange = (event: React.ChangeEvent<{ value: string }>) => {
+    setFirstSelector(event.target.value);
+    setSecondSelector("");
+  };
+
+  // eslint-disable-next-line require-jsdoc
+  const handleSecondSelectorChange = (event: React.ChangeEvent<{ value: string }>) => {
+    setSecondSelector(event.target.value);
+  };
+
+  return (
+    <FormControlWrapper>
+      <Selector
+        value={firstSelector}
+        onChange={handleFirstSelectorChange}
+      >
+        <option value="">Select user</option>
+        <option value="1">User 1</option>
+        <option value="2">User 2</option>
+        <option value="3">User 3</option>
+      </Selector>
+      <Selector
+        value={secondSelector}
+        onChange={handleSecondSelectorChange}
+        disabled={firstSelector === ""}
+      >
+        <option value="">Select user</option>
+        <option value="1">User 1</option>
+        <option value="2">User 2</option>
+        <option value="3">User 3</option>
+      </Selector>
+      <Selector>
+        <option value="">Select user</option>
+        <option value="1">User 1</option>
+        <option value="2">User 2</option>
+        <option value="3">User 3</option>
+      </Selector>
+    </FormControlWrapper>
+  );
+};
+
+export default UserSelector;
