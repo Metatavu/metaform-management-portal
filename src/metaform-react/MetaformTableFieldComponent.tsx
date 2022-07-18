d79561c8619b9936096fe3137763508cdcc6a17a
Eetu Lepistö <bodom02@gmail.com>
2022-07-18 17:43:11 +0300

    Translated table field

--- src/metaform-react/MetaformTableFieldComponent.tsx
+++ src/metaform-react/MetaformTableFieldComponent.tsx
@@ -1,7 +1,9 @@
-/* eslint-disable */ // Remove when refactoring is done
+import { Button, TableBody, TableCell, TableHead, TableRow } from "@mui/material";
+import strings from "localization/strings";
 import React, { ReactNode } from "react";
+import { TableNumberCellWrapper, TableTextCellWrapper } from "styled/react-components/react-components";
 import { MetaformField, MetaformTableColumn, MetaformTableColumnType } from "../generated/client/models";
-import { FieldValue, IconName, Strings, TableFieldValue, TableFieldRowValue, TableFieldCellValue } from "./types";
+import { FieldValue, IconName, TableFieldValue, TableFieldRowValue, TableFieldCellValue } from "./types";
 
 /**
  * Component props
@@ -15,101 +17,82 @@ interface Props {
 }
 
 /**
- * Component state
- */
-interface State {
-  rowValues : TableFieldValue;
-}
-
-/**
  * Component for table field
  */
-export class MetaformTableFieldComponent extends React.Component<Props, State> {
+export const MetaformTableFieldComponent: React.FC<Props> = ({
+  field,
+  formReadOnly,
+  value,
+  renderIcon,
+  onValueChange
+}) => {
+  const [ rowValues, setRowValues ] = React.useState<TableFieldValue>(value as TableFieldValue || [{}]);
+
+  if (!field.name) {
+    return null;
+  }
 
   /**
-   * Constructor
+   * Sets draft and state data
    * 
-   * @param props component props
+   * @param newRowValues new row values
    */
-  constructor(props: Props) {
-    super(props);
-
-    this.state = {
-      rowValues : this.props.value as TableFieldValue || [{}]
-    };
-  }
+  const onTableValueChange = (newRowValues: TableFieldValue) => {
+    const newTableValues = newRowValues as TableFieldValue;
+    setRowValues(newTableValues);
+    onValueChange(newTableValues);
+  };
 
   /**
-   * Component render method
+   * Event handler for add button click
    */
-  public render() {
-    const { field } = this.props;
+  const onAddRowButtonClick = () => {
+    const newRowValues = [...rowValues];
 
-    if (!field.name) {
-      return null;
-    }
-
-    return (
-      <div>
-        <table style={{ width: "100%" }}>
-          { this.renderHeader() }
-          { this.renderBody() }
-        </table>
-        <div>
-          { this.renderAddRowButton() }
-        </div>
-      </div>
-    );
-  }
+    newRowValues.push({});
+    onTableValueChange(newRowValues);
+  };
 
   /**
-   * Renders table header
-   */
-  private renderHeader = () => {
-    const { field } = this.props;
-    const columns = field.columns || [];
-    
-    return (
-      <thead>
-        <tr>
-          { columns.map(this.renderHeaderColumn) }
-        </tr>
-      </thead>
-    );
-  }
-
-  /**
-   * Renders table header column
+   * Sets cell value
    * 
+   * @param rowNumber row number
    * @param column column
+   * @param cellValue new value
    */
-  private renderHeaderColumn = (column: MetaformTableColumn) => {
-    return (
-      <th style={{ textAlign: "left" }} key={ column.name }> { column.title } </th>
-    );
-  }
+  const setTableFieldCellValue = (rowNumber: number, column: MetaformTableColumn, cellValue: TableFieldCellValue) => {
+    const newRowValues = [ ...rowValues ];
+
+    newRowValues[rowNumber][column.name] = cellValue;
+    onTableValueChange(newRowValues);
+  };
 
   /**
-   * Renders table body
+   * Event handler for table text cell input change
+   * 
+   * @param rowNumber number of row
+   * @param column column
+   * @param cellValue new value
    */
-  private renderBody = () => {
-    const { rowValues } = this.state;
+  const onCellInputTextChange = (rowNumber: number, column: MetaformTableColumn, cellValue: string) => {
+    setTableFieldCellValue(rowNumber, column, cellValue);
+  };
 
-    return (
-      <tbody>
-        {
-          rowValues.map(this.renderRow)
-        }
-      </tbody>
-    );
-  }
+  /**
+   * Event handler for table number cell input change
+   * 
+   * @param rowNumber number of row
+   * @param column column
+   * @param cellValue new value
+   */
+  const onCellInputNumberChange = (rowNumber: number, column: MetaformTableColumn, cellValue: string) => {
+    setTableFieldCellValue(rowNumber, column, parseFloat(cellValue));
+  };
 
   /**
    * Renders an add row button
    */
-  private renderAddRowButton = () => {
-    const { field, formReadOnly, renderIcon } = this.props;
-
+  const renderAddRowButton = () => {
     if (field.addRows === false) {
       return null;
     }
@@ -119,12 +102,98 @@ export class MetaformTableFieldComponent extends React.Component<Props, State> {
     }
     
     return (
-      <a style={{ cursor: "pointer" }} onClick={ this.onAddRowButtonClick }>
+      <Button style={{ cursor: "pointer" }} onClick={ onAddRowButtonClick }>
         { renderIcon("add", "add") }
-        <span style={{ verticalAlign: "super" }}>Lisää uusi rivi</span>
-      </a>
+        <span style={{ verticalAlign: "super" }}>{ strings.fieldComponents.tableField.addNewRow }</span>
+      </Button>
     );
-  }
+  };
+
+  /**
+   * Renders text cell input element
+   * 
+   * @param rowNumber row number
+   * @param column column
+   * @param cellValue value
+   */
+  const renderCellInputText = (rowNumber: number, column: MetaformTableColumn, cellValue: TableFieldCellValue) => {
+    return (
+      <TableTextCellWrapper
+        value={ cellValue || "" }
+        key={ column.name }
+        InputProps={{
+          readOnly: formReadOnly || field.readonly,
+          disableUnderline: true
+        }}
+        onChange={ (event: React.ChangeEvent<HTMLInputElement>) => onCellInputTextChange(rowNumber, column, event.target.value) }
+      />
+    );
+  };
+
+  /**
+   * Renders number cell input element
+   * 
+   * @param rowNumber row number
+   * @param column column
+   * @param cellValue value
+   */
+  const renderCellInputNumber = (rowNumber: number, column: MetaformTableColumn, cellValue: TableFieldCellValue) => {
+    return (
+      <TableNumberCellWrapper
+        value={ cellValue || "" }
+        type="number"
+        key={ column.name }
+        InputProps={{
+          readOnly: formReadOnly || field.readonly,
+          disableUnderline: true
+        }}
+        onChange={ (event: React.ChangeEvent<HTMLInputElement>) => onCellInputNumberChange(rowNumber, column, event.target.value) }
+      />
+    );
+  };
+
+  /**
+   * Renders cell input element
+   * 
+   * @param rowNumber row number
+   * @param column column
+   * @param cellValue value
+   */
+  const renderCellInput = (rowNumber: number, column: MetaformTableColumn, cellValue: TableFieldCellValue) => {
+    switch (column.type) {
+      case MetaformTableColumnType.Text:
+        return renderCellInputText(rowNumber, column, cellValue);
+      case MetaformTableColumnType.Number:
+        return renderCellInputNumber(rowNumber, column, cellValue);
+      default:
+        return (
+          <div style={{ color: "red" }}>
+            {" "}
+            { strings.fieldComponents.tableField.unknownColumnType }
+            {" "}
+            { column.type }
+            {" "}
+          </div>
+        );
+    }
+  };
+
+  /**
+   * Renders cell element
+   * 
+   * @param rowNumber row number
+   * @param column column
+   * @param cellValue value
+   */
+  const renderCell = (rowNumber: number, column: MetaformTableColumn, cellValue: TableFieldCellValue) => {
+    return (
+      <TableCell sx={{ padding: 0 }} key={ column.name }>
+        {
+          renderCellInput(rowNumber, column, cellValue)
+        }
+      </TableCell>
+    );
+  };
 
   /**
    * Renders row element
@@ -132,151 +201,78 @@ export class MetaformTableFieldComponent extends React.Component<Props, State> {
    * @param rowNumber row number
    * @param rowValue row value
    */
-  private renderRow = (rowValue: TableFieldRowValue, rowNumber: number) => {
-    const { field } = this.props;
+  const renderRow = (rowValue: TableFieldRowValue, rowNumber: number) => {
     const columns = field.columns || [];
 
     return (
-      <tr key={ rowNumber }>
+      <TableRow key={ rowNumber }>
         {
           columns.map(column => {
-            return this.renderCell(rowNumber, column, rowValue[column.name] || null);
+            return renderCell(rowNumber, column, rowValue[column.name] || null);
           })
         }
-      </tr>
+      </TableRow>
     );
-  }
+  };
 
   /**
-   * Renders cell element
-   * 
-   * @param rowNumber row number
-   * @param column column
-   * @param value value
+   * Renders table body
    */
-  private renderCell = (rowNumber: number, column: MetaformTableColumn, value: TableFieldCellValue) => {
-    return (
-      <td key={ column.name }>
-        {
-          this.renderCellInput(rowNumber, column, value)
-        }
-      </td>
-    );
-  }
-
-  /**
-   * Renders cell input element
-   * 
-   * @param rowNumber row number
-   * @param column column
-   * @param value value
-   */
-  private renderCellInput = (rowNumber: number, column: MetaformTableColumn, value: TableFieldCellValue) => {
-    switch (column.type) {
-      case MetaformTableColumnType.Text:
-        return this.renderCellInputText(rowNumber, column, value);
-      case MetaformTableColumnType.Number:
-        return this.renderCellInputNumber(rowNumber, column, value);
-      default:
-        return <div style={{ color: "red" }}> Unknown column type { column.type } </div>;
+  const renderBody = () => {
+    if (!rowValues) {
+      return null;
     }
-  }
-
-  /**
-   * Renders text cell input element
-   * 
-   * @param rowNumber row number
-   * @param column column
-   * @param value value
-   */
-  private renderCellInputText = (rowNumber: number, column: MetaformTableColumn, value: TableFieldCellValue) => {
-    const { field, formReadOnly } = this.props;
 
     return (
-      <input 
-        value={ value || "" } 
-        key={ column.name } 
-        style={{ width: "100%", padding: 5 }} 
-        readOnly={ formReadOnly || field.readonly }
-        onChange={ (event: React.ChangeEvent<HTMLInputElement>) => this.onCellInputTextChange(rowNumber, column, event.target.value) }
-      />
+      <TableBody>
+        {
+          rowValues.map((rowValue, rowNumber) => {
+            return renderRow(rowValue, rowNumber);
+          })
+        }
+      </TableBody>
     );
-  }
+  };
 
   /**
-   * Renders number cell input element
+   * Renders table header column
    * 
-   * @param rowNumber row number
    * @param column column
-   * @param value value
    */
-  private renderCellInputNumber = (rowNumber: number, column: MetaformTableColumn, value: TableFieldCellValue) => {
-    const { field, formReadOnly } = this.props;
+  const renderHeaderColumn = (column: MetaformTableColumn) => {
+    return (
+      <TableCell sx={{ textAlign: "left", padding: 0 }} key={ column.name }>
+        { column.title }
+      </TableCell>
+    );
+  };
+
+  /**
+   * Renders table header
+   */
+  const renderHeader = () => {
+    const columns = field.columns || [];
     
     return (
-      <input 
-        value={ value || "" }
-        type={ "number" } 
-        key={ column.name } 
-        style={{ width: "100%", padding: 5 }} 
-        readOnly={ formReadOnly || field.readonly }
-        onChange={ (event: React.ChangeEvent<HTMLInputElement>) => this.onCellInputNumberChange(rowNumber, column, event.target.value) }
-      />
+      <TableHead>
+        <TableRow>
+          { columns.map(renderHeaderColumn) }
+        </TableRow>
+      </TableHead>
     );
-  }
+  };
 
-  /**
-   * Sets cell value
-   * 
-   * @param rowNumber row number
-   * @param column column
-   * @param value new value
-   */
-  private setTableFieldCellValue = (rowNumber: number, column: MetaformTableColumn, value: TableFieldCellValue) => {
-    const rowValues = [ ...this.state.rowValues ];
+  return (
+    <div>
+      <table style={{ width: "100%" }}>
+        { renderHeader() }
+        { renderBody() }
+      </table>
+      <div>
+        { renderAddRowButton() }
+      </div>
+    </div>
+  );
+};
 
-    rowValues[rowNumber][column.name] = value;
-    this.onTableValueChange(rowValues);
-  }
-
-  /**
-   * Event handler for field input change
-   * 
-   * @param event event
-   */
-  private onCellInputTextChange = (rowNumber: number, column: MetaformTableColumn, value: string) => {
-    this.setTableFieldCellValue(rowNumber, column, value);
-  }
-
-  /**
-   * Event handler for field input change
-   * 
-   * @param event event
-   */
-  private onCellInputNumberChange = (rowNumber: number, column: MetaformTableColumn, value: string) => {
-    this.setTableFieldCellValue(rowNumber, column, parseFloat(value));
-  }
-
-  /**
-   * Event handler for add button click
-   */
-  private onAddRowButtonClick = () => {
-    const { rowValues } = this.state;
-
-    rowValues.push({});
-    this.onTableValueChange(rowValues);
-  }
-
-  /**
-   * Sets draft and state data
-   * 
-   * @param rowValues row values
-   */
-  private onTableValueChange = (rowValues: TableFieldValue) => {
-    const { onValueChange } = this.props;
-
-    this.setState({rowValues: rowValues});
-    onValueChange(rowValues);
-  }
-
-}
+export default MetaformTableFieldComponent;
