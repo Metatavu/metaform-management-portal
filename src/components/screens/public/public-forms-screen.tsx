6b0925c531547f7d5180329e7ae4b0b65ba0af07
Otto Hooper <otto.hooper@metatavu.fi>
2022-07-26 10:24:50 +0300

    Very basic list that links the form name

--- src/components/screens/public/public-forms-screen.tsx
+++ src/components/screens/public/public-forms-screen.tsx
@@ -1,12 +1,27 @@
-import { Typography } from "@mui/material";
-import strings from "localization/strings";
+import { Grid, List, ListItem, ListItemText, Typography } from "@mui/material";
 import React from "react";
+import { Link } from "react-router-dom";
 
 /**
  * Public forms screen component
  */
 const PublicFormsScreen: React.FC = () => (
-  <Typography>{ strings.generic.notImplemented }</Typography>
+  <Grid container spacing={ 2 }>
+    <Grid item xs={ 12 }>
+      <Typography>
+        Public forms
+      </Typography>
+      <List>
+        <ListItem>
+          <ListItemText primary={
+            <Typography>
+              <Link to="/forms/1">Form 1</Link>
+            </Typography>}
+          />
+        </ListItem>
+      </List>
+    </Grid>
+  </Grid>
 );
 
 export default PublicFormsScreen;
