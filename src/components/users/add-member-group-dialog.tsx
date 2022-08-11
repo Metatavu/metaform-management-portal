2b8c67a337a5f9eb9ba8cab98f5ff5d5b2f0a174
Eetu Lepistö <bodom02@gmail.com>
2022-08-11 16:03:10 +0300

    Added generic loader and implemented it on users screen

--- src/components/users/add-member-group-dialog.tsx
+++ src/components/users/add-member-group-dialog.tsx
@@ -3,25 +3,28 @@ import * as React from "react";
 import strings from "../../localization/strings";
 import { useState } from "react";
 import { Button, Dialog, DialogActions, DialogContent, DialogContentText, DialogTitle, TextField } from "@mui/material";
+import GenericLoaderWrapper from "components/generic/generic-loader";
 
 /**
  * Interface representing component properties
  */
 interface Props {
+  loading: boolean;
   open: boolean;
   onCancel: () => void;
-  onCreate: (displayName: string) => void;
+  onCreate: (displayName: string | undefined) => void;
 }
 
 /**
  * React component for add member group dialogs
  */
 const AddMemberGroupDialog: React.FC<Props> = ({
+  loading,
   open,
   onCancel,
   onCreate
 }) => {
-  const [ displayName, setDisplayName ] = useState("");
+  const [ displayName, setDisplayName ] = useState<string | undefined>();
 
   /**
    * Event handler for display name change
@@ -38,13 +41,21 @@ const AddMemberGroupDialog: React.FC<Props> = ({
   const onCreateClick = () => {
     onCreate(displayName);
   };
+
+  /**
+   * Event handler for cancel click
+   */
+  const onCancelClick = () => {
+    setDisplayName(undefined);
+    onCancel();
+  };
   
   if (!open) {
     return null;
   }
 
   return (
-    <Dialog open={ true } onClose={ onCancel }>
+    <Dialog open={ true } onClose={ onCancelClick }>
       <DialogTitle variant="h2">{ strings.userManagementScreen.addMemberGroupDialog.title }</DialogTitle>
       <DialogContent style={{ width: 500 }}>
         <DialogContentText variant="body1" color="#ccc" style={{ paddingBottom: 16 }}>
@@ -59,12 +70,14 @@ const AddMemberGroupDialog: React.FC<Props> = ({
         />
       </DialogContent>
       <DialogActions style={{ padding: 24, paddingTop: 0 }}>
-        <Button disableElevation variant="contained" onClick={ onCancel } color="secondary" autoFocus>
+        <Button disableElevation variant="contained" onClick={ onCancelClick } color="secondary" autoFocus>
           { strings.userManagementScreen.addMemberGroupDialog.cancelButton }
         </Button>
-        <Button onClick={ onCreateClick } color="primary" disabled={ !displayName }>
-          { strings.userManagementScreen.addMemberGroupDialog.createButton }
-        </Button>
+        <GenericLoaderWrapper loading={ loading }>
+          <Button onClick={ onCreateClick } color="primary" disabled={ !displayName }>
+            { strings.userManagementScreen.addMemberGroupDialog.createButton }
+          </Button>
+        </GenericLoaderWrapper>
       </DialogActions>
     </Dialog>
   );
