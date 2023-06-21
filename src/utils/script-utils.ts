1a4cb072595bd1598135a41b743836449fe3f98e
Simeon <simeon.platonov@gmail.com>
2023-06-21 11:34:16 +0300

    Removed test script

--- src/utils/script-utils.ts
+++ src/utils/script-utils.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import ExcelJS from "exceljs";
 
 namespace ScriptUtils {
@@ -16,48 +17,8 @@ namespace ScriptUtils {
   export const runScriptOnSpreadsheet = async (spreadsheetFile: Blob, script: string, nameClassifierEntries: NameClassifierEntry[]) => {
     const workbook = new ExcelJS.Workbook();
     await workbook.xlsx.read(spreadsheetFile.stream());
-
-    const totalOfAppliedAidFields = nameClassifierEntries.filter(entry => entry.classifier === "total-of-applied-aid");
-    const numberOfPracticesFields = nameClassifierEntries.filter(entry => entry.classifier === "number-of-practices");
-    const numberOfCompetitionsFields = nameClassifierEntries.filter(entry => entry.classifier === "number-of-competitions");
-
-    const mainSheet = workbook.worksheets[0];
-    mainSheet.columns.forEach((column, columnIndex) => {
-      const columnTitle = column.values ? column.values[1] : undefined;
-      const isTotalOfAppliedAid = !!totalOfAppliedAidFields.find(field => field.name === columnTitle?.toString());
-      const isNumberOfPractises = !!numberOfPracticesFields.find(field => field.name === columnTitle?.toString());
-      const isNumberOfCompetitions = !!numberOfCompetitionsFields.find(field => field.name === columnTitle?.toString());
-      
-      if (isTotalOfAppliedAid || isNumberOfPractises || isNumberOfCompetitions) {
-        const sumColumn: any[] = [`Summa - ${columnTitle?.toString()}`];
-        const currentValues = column.values?.map(value => value as any) || [];
-        currentValues.shift();
-        mainSheet.spliceColumns(columnIndex + 1, 1, currentValues, sumColumn);
-        column.values?.forEach((value, row) => {
-          const tableName = value?.toString();
-          if (tableName && row !== 1) {
-            const tableSheet = workbook.worksheets.find(sheet => sheet.name === tableName);
-
-            if (isTotalOfAppliedAid) {
-              const cellAddress = tableSheet!.lastRow!.getCell("B").address;
-              mainSheet.getCell(row, columnIndex + 2).value = { formula: `='${tableName}'!${cellAddress}`, date1904: false };
-            }
-
-            if (isNumberOfPractises || isNumberOfCompetitions) {
-              const newRowCount = tableSheet!.rowCount + 1;
-              for (let i = 1; i < newRowCount; i++) {
-                tableSheet!.getCell(`C${i}`).value = { formula: `=A${i}*B${i}`, date1904: false };
-              }
-              
-              tableSheet!.getCell(`C${newRowCount}`).value = { formula: `=SUM(C1:C${newRowCount - 1})`, date1904: false };
-              mainSheet.getCell(row, columnIndex + 2).value = { formula: `='${tableName}'!C${newRowCount}`, date1904: false };
-            }
-          }
-        });
-      }
-    });
     // eslint-disable-next-line no-eval
-    eval(`${script}`);
+    eval(script);
     const newFile = new Blob([await workbook.xlsx.writeBuffer()]);
     return newFile;
   };
