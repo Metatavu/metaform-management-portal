1aad56a993643238d57dbb8e1e06ffc5e5635d53
Jdallos <joshdallos@gmail.com>
2023-10-31 12:37:35 +0200

    Field rendered according to schedule

---
+++ src/utils/schedule-utils.ts
@@ -0,0 +1,47 @@
+import { MetaformFieldSchedule } from "generated/client";
+import { DateTime } from "luxon";
+
+/**
+ * Utility class for field schedules
+ */
+namespace ScheduleUtils {
+
+  /**
+   * Returns whether field is scheduled to be visible or not
+   *
+   * @param schedule MetaformFieldSchedule
+   * @returns whether field is scheduled to be visible or not
+   */
+  export const isVisible = (schedule: MetaformFieldSchedule | undefined) => {
+    if (schedule === undefined) return true;
+
+    const { startTime, endTime } = schedule;
+
+    const currentTime = DateTime.local();
+
+    if (!startTime && !endTime) {
+      return true;
+    }
+
+    if (startTime) {
+      const startDateTime = DateTime.fromJSDate(new Date(startTime));
+
+      if (!endTime) {
+        const endDateTime = DateTime.local(2100, 1, 1);
+        return startDateTime <= currentTime && currentTime < endDateTime;
+      }
+
+      if (endTime) {
+        const endDateTime = DateTime.fromJSDate(new Date(endTime));
+        return startDateTime <= currentTime && currentTime < endDateTime;
+      }
+    } else if (endTime) {
+      const endDateTime = DateTime.fromJSDate(new Date(endTime));
+      return currentTime < endDateTime;
+    }
+    return true;
+  };
+
+}
+
+export default ScheduleUtils;
